da117392e4cb354fc3f62927faf37c9c0c61d1b5
pm72 <46081859+pm72@users.noreply.github.com>
2023-10-03 12:07:06 +0400

    pm72

--- personal_portfolio/portfolio/views.py
+++ personal_portfolio/portfolio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Project
 
+
 def home(request):
   projects = Project.objects.all()
 
--- store/products/views.py
+++ store/products/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
-  return render(request, 'products/index.html')
+  context = {
+    'title': 'Store',
+  }
+
+  return render(request, 'products/index.html', context)
+
 
 def products(request):
-  return render(request, 'products/products.html')
+  context = {
+    'title': 'Store - Каталог',
+  }
+
+  return render(request, 'products/products.html', context)
